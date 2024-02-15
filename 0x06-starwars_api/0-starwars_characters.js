071b15b4a97a63f129e1691c0c59cded0e616298
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-02-15 23:51:25 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
-	const util = require('util');
-	const request = util.promisify(require('request'));
-	const filmID = process.argv[2];
 
-	async function starwarsCharacters (filmId) {
-	  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-	  let response = await (await request(endpoint)).body;
-	  response = JSON.parse(response);
-	  const characters = response.characters;
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-	  for (let i = 0; i < characters.length; i++) {
-	    const urlCharacter = characters[I];
-	    let character = await (await request(urlCharacter)).body;
-	    character = JSON.parse(character);
-	    console.log(character.name);
-	  }
-	}
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i]; // Changed "I" to "i" for correct indexing
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
+}
 
 starwarsCharacters(filmID);
-
