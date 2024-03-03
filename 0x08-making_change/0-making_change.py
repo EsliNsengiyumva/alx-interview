27de1f2817df9d8ba9141d4be9b03ded4de1db38
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-03-03 09:18:23 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
+"""
+Chamge comes from within
+"""
+
 
 def makeChange(coins, total):
     """
-    Calculates the fewest number of coins needed to meet a given total amount.
-
-    Args:
-        coins (list): A list of coin denominations available.
-        total (int): The target total amount.
-
-    Returns:
-        int: The fewest number of coins needed to meet the total amount.
-             Returns -1 if the total cannot be met by any combination of coins.
+    Given a pile of cons of different values,
+    determine the fewest number of cons needed to meet a given amount total.
     """
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    if total <= 0:
+        return 0
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
-        for j in range(coin, total + 1):
-            dp[j] = min(dp[j], dp[j - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
