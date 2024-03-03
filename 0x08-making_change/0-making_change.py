f9b76f40c7ab92f5006d533dfcea9550038f070c
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2024-03-02 23:04:03 -0800

    update file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
 def makeChange(coins, total):
+    """
+    Calculates the fewest number of coins needed to meet a given total amount.
+
+    Args:
+        coins (list): A list of coin denominations available.
+        total (int): The target total amount.
+
+    Returns:
+        int: The fewest number of coins needed to meet the total amount.
+             Returns -1 if the total cannot be met by any combination of coins.
+    """
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
