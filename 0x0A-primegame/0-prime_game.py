b920159727c1b524b25f282c8a060da30211e489
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-04-01 21:07:13 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 """
 Prime Game
 """
 
-
 def is_prime(num):
+    """
+    Check if a number is prime.
+    """
     if num < 2:
         return False
     for i in range(2, int(num ** 0.5) + 1):
@@ -13,16 +14,20 @@ def is_prime(num):
             return False
     return True
 
-
 def winner_of_round(n):
+    """
+    Determine the winner of a round based on the number of primes.
+    """
     primes = [i for i in range(2, n + 1) if is_prime(i)]
     if len(primes) % 2 == 0:
         return "Ben"
     else:
         return "Maria"
 
-
 def isWinner(x, nums):
+    """
+    Determine the overall winner based on the results of multiple rounds.
+    """
     maria_wins = 0
     ben_wins = 0
 
