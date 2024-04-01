96472b2636a0026d092fe6c0f3674e4008437df1
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-04-01 21:17:33 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,31 +3,51 @@
 Prime Game
 """
 
-def is_prime(num):
-    """
-    Check if a number is prime.
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-def winner_of_round(n):
-    """
-    Determine the winner of a round based on the number of primes.
-    """
-    primes = [i for i in range(2, n + 1) if is_prime(i)]
-    if len(primes) % 2 == 0:
-        return "Ben"
-    else:
-        return "Maria"
-
 def isWinner(x, nums):
     """
-    Determine the overall winner based on the results of multiple rounds.
+    Determines the winner of the prime game.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list): An array of n for each round.
+
+    Returns:
+        str: The name of the player that won the most rounds.
+             If the winner cannot be determined, returns None.
     """
+    def is_prime(num):
+        """
+        Checks if a number is prime.
+
+        Args:
+            num (int): The number to check.
+
+        Returns:
+            bool: True if the number is prime, False otherwise.
+        """
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def winner_of_round(n):
+        """
+        Determines the winner of a single round.
+
+        Args:
+            n (int): The upper limit of the numbers for the round.
+
+        Returns:
+            str: The name of the winner (Maria or Ben).
+        """
+        primes = [i for i in range(2, n + 1) if is_prime(i)]
+        if len(primes) % 2 == 0:
+            return "Ben"
+        else:
+            return "Maria"
+
     maria_wins = 0
     ben_wins = 0
 
@@ -44,3 +64,4 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
+
