e3c78aaf58b1a462a0528e7de176d72311871adb
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-04-01 20:59:14 +0200

    Update main_0.py

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -3,4 +3,4 @@
 import_name = __import__('0-prime_game')
 is_winner = import_name.isWinner
 
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(is_winner(5, [2, 5, 1, 4, 3])))
