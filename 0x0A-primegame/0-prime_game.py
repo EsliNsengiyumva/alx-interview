9bddb31082fd51416854d6a43a63dec6f61cb949
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-04-01 20:57:23 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
+
 """
-Game of a prime number
+Prime Game
 """
 
 
 def is_prime(num):
-    """
-    Check if a number is prime.
-
-    Args:
-        num (int): The number to check for primality.
-
-    Returns:
-        bool: True if the number is prime, False otherwise.
-    """
     if num < 2:
         return False
     for i in range(2, int(num ** 0.5) + 1):
@@ -23,15 +15,6 @@ def is_prime(num):
 
 
 def winner_of_round(n):
-    """
-    Determine the winner of a round based on the number of primes.
-
-    Args:
-        n (int): The number representing the round.
-
-    Returns:
-        str: The winner of the round ("Maria" or "Ben").
-    """
     primes = [i for i in range(2, n + 1) if is_prime(i)]
     if len(primes) % 2 == 0:
         return "Ben"
@@ -40,17 +23,6 @@ def winner_of_round(n):
 
 
 def isWinner(x, nums):
-    """
-    Determine the overall winner of multiple rounds.
-
-    Args:
-        x (int): Not used in the function.
-        nums (list): A list of numbers representing the rounds.
-
-    Returns:
-        str or None: The overall winner ("Maria" or "Ben")
-        , or None in case of a tie.
-    """
     maria_wins = 0
     ben_wins = 0
 
