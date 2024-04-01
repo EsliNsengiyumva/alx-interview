1f40e7e1a67b2f696d585cf05027b187494db569
Esli Nsengiyumva <47382543+EsliNsengiyumva@users.noreply.github.com>
2024-04-01 21:13:25 +0200

    Update main_0.py

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-import_name = __import__('0-prime_game')
-isWinner = import_name.isWinner
+"""
+Script to determine the winner of a prime game.
+"""
 
+# Import the isWinner function from the 0-prime_game module
+isWinner = __import__('0-prime_game').isWinner
+
+# Print the winner returned by calling isWinner function with arguments 5 and [2, 5, 1, 4, 3]
 print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
